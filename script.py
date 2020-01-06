4c870940f343330789016e2275ddb59df26cab54
Ken Li <krli@ualberta.ca>
2020-01-06 16:00:26 -0700

    Print Code from GitHub

--- script.py
+++ script.py
@@ -1,8 +1,13 @@
 import requests
 
-print("requests version: %s" % requests.__version__)
+# print("requests version: %s" % requests.__version__)
 
-r = requests.get('http://www.google.com')
+# r = requests.get('http://www.google.com')
 
-print("Status code: %s" % r.status_code)
-print("URL Content: %s" % r.text)
+# print("Status code: %s" % r.status_code)
+# print("URL Content: %s" % r.text)
+
+r = requests.get('https://raw.githubusercontent.com/KenRLi/CMPUT404-Lab1/master/script.py')
+
+# print("Status code: %s" % r.status_code)
+print("GitHub Content:\n---\n%s\n---" % r.text)
