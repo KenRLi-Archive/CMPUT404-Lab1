12a2986ad8b41f3c02e5299bdec6d3983414c49f
Ken Li <krli@ualberta.ca>
2020-01-07 08:19:03 -0700

    Added comments for each code block

--- script.py
+++ script.py
@@ -3,14 +3,15 @@
 
 import requests
 
-# print("requests version: %s" % requests.__version__)
+# Question 2 & 3
+print("requests version: %s" % requests.__version__)
 
-# r = requests.get('http://www.google.com')
+# curl - step 5
+r = requests.get('http://www.google.com')
+print("Status code: %s" % r.status_code)
+print("URL Content: %s" % r.text)
 
-# print("Status code: %s" % r.status_code)
-# print("URL Content: %s" % r.text)
-
+# curl - step 10
 r = requests.get('https://raw.githubusercontent.com/KenRLi/CMPUT404-Lab1/master/script.py')
-
-# print("Status code: %s" % r.status_code)
+print("Status code: %s" % r.status_code)
 print("GitHub Content:\n---\n%s---" % r.text)
